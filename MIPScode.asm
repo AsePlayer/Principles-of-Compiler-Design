ce4ed42ff7b43cd9f1259b8d851747cbaedb3a57
AsePlayer <aseplayerofficial@hotmail.com>
2023-02-12 21:08:13 -0700

    Testing IR Code

--- MIPScode.asm
+++ MIPScode.asm
@@ -11,3 +11,29 @@ syscall      # system call (terminate)
 li $v0,10   # call code for terminate
 syscall      # system call (terminate)
 .end main
+.text
+main:
+# -----------------------
+li $t0,1
+move $a0,$t0
+# -----------------
+#  Done, terminate program.
+
+li $v0,1   # call code for terminate
+syscall      # system call (terminate)
+li $v0,10   # call code for terminate
+syscall      # system call (terminate)
+.end main
+.text
+main:
+# -----------------------
+li $t0,1
+move $a0,$t0
+# -----------------
+#  Done, terminate program.
+
+li $v0,1   # call code for terminate
+syscall      # system call (terminate)
+li $v0,10   # call code for terminate
+syscall      # system call (terminate)
+.end main
