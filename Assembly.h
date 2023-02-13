e1386c9494bc632488b07b3b5992ed9435baf3e1
AsePlayer <aseplayerofficial@hotmail.com>
2023-02-12 20:48:02 -0700

    Added Assembly functionality

---
+++ Assembly.h
@@ -0,0 +1,52 @@
+
+// Set of functions to emit MIPS code
+FILE * MIPScode;
+
+void  initAssemblyFile(){
+    // Creates a MIPS file with a generic header that needs to be in every file
+
+    MIPScode = fopen("MIPScode.asm", "a");
+    
+    fprintf(MIPScode, ".text\n");
+    fprintf(MIPScode, "main:\n");
+    fprintf(MIPScode, "# -----------------------\n");
+
+}
+
+void emitMIPSAssignment(char * id1, char * id2){
+  // This is the temporary approach, until register management is implemented
+
+  fprintf(MIPScode, "li $t1,%s\n", id1);
+  fprintf(MIPScode, "li $t2,%s\n", id2);
+  fprintf(MIPScode, "li $t2,$t1\n");
+}
+
+void emitMIPSConstantIntAssignment (char id1[50], char id2[50]){
+     // This is the temporary approach, until register management is implemented
+     // The parameters of this function should inform about registers
+     // For now, this is "improvised" to illustrate the idea of what needs to 
+     // be emitted in MIPS
+
+     // nextRegister = allocateRegister(id1);  // This is conceptual to inform what needs to be done later
+
+    fprintf(MIPScode, "li $t0,%s\n", id2);
+}
+
+void emitMIPSWriteId(char * id){
+    // This is what needs to be printed, but must manage registers
+    // $a0 is the register through which everything is printed in MIPS
+    
+    //fprintf(MIPScode, "li $a0,%s\n", id);
+    fprintf(MIPScode, "move $a0,%s\n", "$t0");
+}
+
+void emitEndOfAssemblyCode(){
+    fprintf(MIPScode, "# -----------------\n");
+    fprintf(MIPScode, "#  Done, terminate program.\n\n");
+    fprintf(MIPScode, "li $v0,1   # call code for terminate\n");
+    fprintf(MIPScode, "syscall      # system call (terminate)\n");
+    fprintf(MIPScode, "li $v0,10   # call code for terminate\n");
+    fprintf(MIPScode, "syscall      # system call (terminate)\n");
+    fprintf(MIPScode, ".end main\n");
+}
+    
---
+++ MIPScode.asm
@@ -0,0 +1,13 @@
+.text
+main:
+# -----------------------
+li $t0,1
+move $a0,$t0
+# -----------------
+#  Done, terminate program.
+
+li $v0,1   # call code for terminate
+syscall      # system call (terminate)
+li $v0,10   # call code for terminate
+syscall      # system call (terminate)
+.end main
